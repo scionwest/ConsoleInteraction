a578f1d042ad53d90279f1018c12680d11c9d12e
Johnathon Sullinger <sully@Johnathons-MacBook-Pro.local>
2017-10-29 17:43:31 -0700

    Initial working example

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleInteraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Line #1");
+            Console.SetCursorPosition(0, Console.CursorTop);
+            Console.WriteLine("New Message");
+            Console.Write("Line #2");
+
+            Console.SetCursorPosition(0, Console.CursorTop);
+            Console.Write("Line #3");
+            Console.SetCursorPosition(0, Console.CursorTop);
+            Console.WriteLine("Second Message");
+            Console.Write("Line #4");
+        }
+    }
+}
